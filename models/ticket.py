2689e1cbebd51de66c2e31b7bcc060ecf0213898
Maayan Kfir <maayan.kfir@appsflyer.com>
2019-04-15 13:06:09 +0300

    fix bugs

--- models/ticket.py
+++ models/ticket.py
@@ -6,18 +6,18 @@ class Ticket:
                  team_name):
         self.ticket_jira_id = ticket_id
         self.ticket_key = ticket_key
-        self.ticket_type = ticket_type
-        self.epic = ticket_epic
-        self.assignee = assignee
-        self.assignee_email = assignee_email
-        self.ticket_status = ticket_status
-        self.ticket_summary = ticket_summary
-        self.ticket_estimation_time = ticket_estimation_time
-        self.ticket_jira_last_update = ticket_last_update
+        self.ticket_type = str(ticket_type).replace("'", "")
+        self.epic = str(ticket_epic).replace("'", "")
+        self.assignee = str(assignee).replace("'", "")
+        self.assignee_email = str(assignee_email).replace("'", "")
+        self.ticket_status = str(ticket_status).replace("'", "")
+        self.ticket_summary = str(ticket_summary).replace("'", "")
+        self.ticket_estimation_time = str(ticket_estimation_time).replace("'", "")
+        self.ticket_jira_last_update = str(ticket_last_update).replace("'", "")
         self.ticket_parent = ticket_parent
         self.ticket_parent_id = ticket_parent_id
         self.ticket_subtasks = ticket_subtasks
-        self.team_name = team_name
+        self.team_name = str(team_name).replace("'", "")
         self.teamgantt_id = None
         self.resource = None
 
